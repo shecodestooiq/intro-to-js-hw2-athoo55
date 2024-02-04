33f1b71dc4955c1c3e4f039c340954dc908500cb
athoo55 <dhwdhwra@gmail.com>
2024-02-05 01:30:47 +0300

    add homework2

---
+++ index.js
@@ -0,0 +1,8 @@
+var a=3;
+var b=5;
+console.log("a>b:", a>b);
+console.log("a<b:", a<b);
+console.log("a>=b:", a>=b);
+console.log("a<=:", a<=b);
+console.log("a==b", a==b);
+console.log("a!=b", a!=b);
